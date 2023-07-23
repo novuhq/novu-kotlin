b6d4ec1728d7d61d0c0fc71bfc62ae0aeaf48cdc
mayorjay <joseolu4gsm@yahoo.com>
2023-07-23 16:33:31 +0100

    feat: Implement other WorkflowGroup Endpoints

---
+++ src/main/kotlin/dto/response/DeleteWorkflowGroupResponse.kt
@@ -0,0 +1,6 @@
+package co.novu.dto.response
+
+data class DeleteWorkflowGroupResponse(
+    var acknowledged: Boolean? = null,
+    var status: String? = null
+)
--- src/test/kotlin/NotificationGroupsApiTest.kt
+++ src/test/kotlin/NotificationGroupsApiTest.kt
@@ -1,10 +1,10 @@
 import co.novu.Novu
 import co.novu.NovuConfig
 import co.novu.dto.request.CreateByNameRequest
+import co.novu.dto.response.DeleteWorkflowGroupResponse
 import co.novu.dto.response.NotificationGroupsResponse
 import co.novu.dto.response.ResponseWrapper
-import co.novu.extensions.createNotificationGroup
-import co.novu.extensions.notificationGroups
+import co.novu.extensions.*
 import com.google.gson.Gson
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -33,7 +33,7 @@ class NotificationGroupsApiTest {
             )
         )
         mockWebServer.enqueue(MockResponse().setResponseCode(200).setBody(Gson().toJson(responseBody)))
-        val response = mockNovu.notificationGroups()
+        val response = mockNovu.getWorkflowGroups()
         val request = mockWebServer.takeRequest()
 
         assert(request.method == "GET")
@@ -54,12 +54,73 @@ class NotificationGroupsApiTest {
         )
         mockWebServer.enqueue(MockResponse().setResponseCode(201).setBody(Gson().toJson(responseBody)))
         val requestBody = CreateByNameRequest(name = "test")
-        val response = mockNovu.createNotificationGroup(requestBody)
+        val response = mockNovu.createWorkflowGroup(requestBody)
         val request = mockWebServer.takeRequest()
 
         assert(request.method == "POST")
         assert(request.path == "/notification-groups")
         assert(request.body.readUtf8() == Gson().toJson(requestBody))
+        assert(response == responseBody)
+    }
+
+    @Test
+    fun testGetWorkflowGroup() = runTest {
+        val responseBody = ResponseWrapper(
+            NotificationGroupsResponse(
+                _id = "1234",
+                name = "test",
+                _environmentId = "environmentId",
+                _organizationId = "organizationId",
+                _parentId = "parentId"
+            )
+        )
+        mockWebServer.enqueue(MockResponse().setResponseCode(201).setBody(Gson().toJson(responseBody)))
+        val id = "id"
+        val response = mockNovu.getWorkflowGroup(id)
+        val request = mockWebServer.takeRequest()
+
+        assert(request.method == "GET")
+        assert(request.path == "/notification-groups/$id")
+        assert(response == responseBody)
+    }
+
+    @Test
+    fun testUpdateWorkflowGroup() = runTest {
+        val responseBody = ResponseWrapper(
+            NotificationGroupsResponse(
+                _id = "1234",
+                name = "test",
+                _environmentId = "environmentId",
+                _organizationId = "organizationId",
+                _parentId = "parentId"
+            )
+        )
+        mockWebServer.enqueue(MockResponse().setResponseCode(201).setBody(Gson().toJson(responseBody)))
+        val id = "id"
+        val requestBody = CreateByNameRequest(name = "test")
+        val response = mockNovu.updateWorkflowGroup(id, requestBody)
+        val request = mockWebServer.takeRequest()
+
+        assert(request.method == "PATCH")
+        assert(request.path == "/notification-groups/$id")
+        assert(request.body.readUtf8() == Gson().toJson(requestBody))
+        assert(response == responseBody)
+    }
+
+    @Test
+    fun testDeleteWorkflowGroup() = runTest {
+        val responseBody = ResponseWrapper(
+            DeleteWorkflowGroupResponse(
+                acknowledged = true
+            )
+        )
+        mockWebServer.enqueue(MockResponse().setResponseCode(201).setBody(Gson().toJson(responseBody)))
+        val id = "id"
+        val response = mockNovu.deleteWorkflowGroup(id)
+        val request = mockWebServer.takeRequest()
+
+        assert(request.method == "DELETE")
+        assert(request.path == "/notification-groups/$id")
         assert(response == responseBody)
     }
 }
