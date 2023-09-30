8bcef4dc50e8bdbfc210cccb60c34a59c2ebc544
mayorjay <joseolu4gsm@yahoo.com>
2023-09-30 04:06:01 +0100

    feat: Create Response helper

--- src/main/kotlin/extensions/BlueprintsExtensions.kt
+++ src/main/kotlin/extensions/BlueprintsExtensions.kt
@@ -4,29 +4,17 @@ import co.novu.Novu
 import co.novu.dto.blueprints.Blueprint
 import co.novu.dto.blueprints.BlueprintsResponse
 import co.novu.dto.response.ResponseWrapper
+import co.novu.helpers.extractResponse
 import mu.KotlinLogging
-import java.lang.Exception
 
 private val logger = KotlinLogging.logger {}
 
 suspend fun Novu.getBlueprintsByCategory(): ResponseWrapper<BlueprintsResponse>? {
     val response = blueprintsApi.getBlueprintsByCategory()
-    response.apply {
-        return if (isSuccessful) {
-            body().apply { logger.debug { this } }
-        } else {
-            throw Exception(errorBody()?.string())
-        }
-    }
+    return response.extractResponse(logger)
 }
 
 suspend fun Novu.getBlueprint(templateId: String): Blueprint? {
     val response = blueprintsApi.getBlueprint(templateId)
-    response.apply {
-        return if (isSuccessful) {
-            body().apply { logger.debug { this } }
-        } else {
-            throw Exception(errorBody()?.string())
-        }
-    }
+    return response.extractResponse(logger)
 }
