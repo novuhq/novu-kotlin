311863be1651f8e79aaccc33100ef82490efb9e3
MayorJay <joseolu4gsm@yahoo.com>
2024-06-19 23:33:55 +0100

    feat: Write docs for Feeds

--- src/main/kotlin/extensions/FeedsExtentions.kt
+++ src/main/kotlin/extensions/FeedsExtentions.kt
@@ -9,16 +9,33 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
+/**
+ * Create a Feed.
+ * @param body an instance of [CreateByNameRequest]
+ * @return [ResponseWrapper] with [FeedResponse] as the response data
+ * @throws [Exception] if a problem occurred talking to the server or if there is a connection error
+ */
 suspend fun Novu.createFeed(body: CreateByNameRequest): ResponseWrapper<FeedResponse>? {
     val response = feedsApi.createFeed(body)
     return response.extractResponse(logger, config.enableLogging)
 }
 
+/**
+ * Retrieve a list of Feeds.
+ * @return [ResponseWrapper] with a list of [FeedResponse] as the response data
+ * @throws [Exception] if a problem occurred talking to the server or if there is a connection error
+ */
 suspend fun Novu.feeds(): ResponseWrapper<List<FeedResponse>>? {
     val response = feedsApi.getFeeds()
     return response.extractResponse(logger, config.enableLogging)
 }
 
+/**
+ * Delete a Feed.
+ * @param feedId the ID of the Feed to be deleted
+ * @return [ResponseWrapper] with [FeedResponse] as the response data
+ * @throws [Exception] if a problem occurred talking to the server or if there is a connection error
+ */
 suspend fun Novu.deleteFeed(feedId: String): ResponseWrapper<FeedResponse>? {
     val response = feedsApi.deleteFeed(feedId)
     return response.extractResponse(logger, config.enableLogging)
