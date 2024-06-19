c9c9153c867c228274e6f0bb84c1f626aa84c7ea
MayorJay <joseolu4gsm@yahoo.com>
2024-06-19 23:28:05 +0100

    feat: Write docs for ExecutionDetails

--- src/main/kotlin/extensions/ExecutionDetailsExtentions.kt
+++ src/main/kotlin/extensions/ExecutionDetailsExtentions.kt
@@ -8,6 +8,13 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
+/**
+ * Retrieve a list of Execution Details.
+ * @param notificationId the ID of a Notification
+ * @param subscriberId the ID of a Subscriber
+ * @return [ResponseWrapper] with a list of [ExecutionDetails] as the response data
+ * @throws [Exception] if a problem occurred talking to the server or if there is a connection error
+ */
 suspend fun Novu.executionDetails(
     notificationId: String,
     subscriberId: String,
