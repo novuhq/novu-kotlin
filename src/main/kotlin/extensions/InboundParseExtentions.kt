1e890ed7de60320af9cb71ccfba8b65aca6dac25
MayorJay <joseolu4gsm@yahoo.com>
2024-06-29 17:24:00 +0100

    feat: Write docs for InboundParse

--- src/main/kotlin/extensions/InboundParseExtentions.kt
+++ src/main/kotlin/extensions/InboundParseExtentions.kt
@@ -7,6 +7,11 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
+/**
+ * Validate the mx record setup for the inbound parse functionality.
+ * @return [ValidateMxRecordSetupForInboundParseResponse]
+ * @throws [Exception] if a problem occurred talking to the server or if there is a connection error
+ */
 suspend fun Novu.validateMxRecordSetupForInboundParse(): ValidateMxRecordSetupForInboundParseResponse? {
     val response = inboundParseApi.validateMxRecordSetupForInboundParse()
     return response.extractResponse(logger, config.enableLogging)
